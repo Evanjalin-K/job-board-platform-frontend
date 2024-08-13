a15960a7f0a33415759dce7ebafdca4002e01e06
Evanjalin-K <jojaev510@gmail.com>
2024-08-14 01:16:42 +0530

    Readme

--- src/services/instance.js
+++ src/services/instance.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const baseURL = "http://localhost:3000"
+const baseURL = ["http://localhost:3000", "https://job-board-platform-frontend.netlify.app", "https://job-board-platform-backend-hw9v.onrender.com"]
 
 const instance = axios.create({
     baseURL,
