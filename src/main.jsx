cda624057d137a9bc32294506f580261e66c1b63
Evanjalin-K <jojaev510@gmail.com>
2024-08-14 00:42:25 +0530

    Update

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,12 @@
-// src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux'; // Import the Provider component
-import { store } from './app/store.js'; // Import your Redux store
+import { Provider } from 'react-redux'; 
+import { store } from './app/store.js'; 
 import App from './App.jsx';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.js';
 
-// Render the App component wrapped with Provider
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
     <App />
